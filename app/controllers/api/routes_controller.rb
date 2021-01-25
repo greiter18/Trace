a9553cace8781d2b221f04ec1b79e6219b4f19e5
gabriel <gabeh.reiter@gmail.com>
2021-01-25 18:53:15 -0500

    heroku push

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -1,27 +1,27 @@
 class Api::RoutesController < ApplicationController
 
 
-  def create
-    @route = Route.create(route_params)
-    render :show
+  # def create
+  #   @route = Route.create(route_params)
+  #   render :show
     
-  end
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
-  def index
-    @routes = current_user.routes.all
-  end
+  # def index
+  #   @routes = current_user.routes.all
+  # end
 
-  def route_params
-    params.require(:route).permit(
-      :title,
-      :start_long,
-      :start_lat,
-      :end_lat,
-      :end_long,
-      :user_id
-    )
+  # def route_params
+  #   params.require(:route).permit(
+  #     :title,
+  #     :start_long,
+  #     :start_lat,
+  #     :end_lat,
+  #     :end_long,
+  #     :user_id
+  #   )
 
 end
