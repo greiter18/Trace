06494eb7e27048fc25472f40954f03f0adf4fdb1
gabriel <gabeh.reiter@gmail.com>
2021-01-26 22:51:37 -0500

    bug fixes

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -1,37 +1,21 @@
 class Api::RoutesController < ApplicationController
 
 
-<<<<<<< HEAD
-  # def create
+  def create
   #   @route = Route.create(route_params)
   #   render :show
-    
   # end
-=======
-  def create
-    @route = Route.create(route_params)
-    render :show
-  end
 
-  def show
-    @route = Routes.find_by(id: params[id])
+  # def show
+  #   @route = Routes.find_by(id: params[:id])
   end
->>>>>>> routes
 
   # def destroy
   # end
 
-<<<<<<< HEAD
-  # def index
-  #   @routes = current_user.routes.all
-  # end
-
-  
-=======
   def index
     @routes = Routes.where(user_id: params[:user_id])
   end
->>>>>>> routes
 
   def route_params
     params.require(:route).permit(
@@ -42,5 +26,5 @@ class Api::RoutesController < ApplicationController
       :end_long,
       :user_id
     )
-
+  end
 end
