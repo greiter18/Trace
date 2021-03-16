687d8dc43273c73977e627157904cf88f5e6c13b
gabriel <gabeh.reiter@gmail.com>
2021-03-16 19:49:32 -0400

    changes to workout form

--- frontend/components/workouts/workout_create.jsx
+++ frontend/components/workouts/workout_create.jsx
@@ -5,8 +5,9 @@ import { Link, withRouter } from 'react-router-dom';
 class WorkoutForm extends React.Component{
   constructor(props){
     super(props)
-    this.state = {title:'',description:'',date:'',time:'', run_type:''}
-    console.log('++++++++++++++++++',this.state)
+    this.state = {title:'',description:'',date:'',time:'', run_type:'',route_id:''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e){
@@ -20,35 +21,50 @@ class WorkoutForm extends React.Component{
     }
   }
 
-  handleTitle(e){
-    this.setState({ title: e.target.value })
-  }
-  handleDescription(e){
-    this.setState({ title: e.target.value })
-  }
-  handleTime(e){
-    this.setState({ title: e.target.value })
-  }
-  handleType(e){
-    this.setState({ title: e.target.value })
-  }
+  // handleTitle(e){
+  //   this.setState({ title: e.target.value })
+  // }
+  // handleDescription(e){
+  //   this.setState({ description: e.target.value })
+  // }
+  // handleDate(e){
+  //   this.setState({ date: e.target.value })
+  // }
+  // handleTime(e){
+  //   this.setState({ time: e.target.value })
+  // }
+  // handleType(e){
+  //   this.setState({ type: e.target.value })
+  // }
+  // handleRoute(e){
+  //   this.setState({ route_id: e.target.value })
+  // }
+  // handleDate(e){
+  //   this.setState({ date: e.target.value })
+  // }
 
 
   render(){
   return (
     <div>
       <WorkoutNav/>
-      {console.log('-----------------state:',this.state)}
+      Create a New Workout
       <form onSubmit={this.handleSubmit}>
-        Title <input type="text" value={this.state.title} onChange={this.handleTitle}/>
-        Description <input type="text" value={this.state.description} onChange={this.handleDescription}/>
-        Time <input type="text" value={this.state.time} onChange={this.handleTime}/>
-        
-        Type <select value={this.state.run_type} onChange={this.handleType}>
-          <option value="run">Run</option>
-          <option value="walk">Walk</option>
+        Title <input type="text" onChange={this.handleChange('title')}/>
+        Date <input type="date" onChange={this.handleChange('date')} 
+        min="2021-01-01" max="2030-12-31"/>
+        Description <input type="text"  onChange={this.handleChange('description')}/>
+        Time <input type="time"  onChange={this.handleChange('time')}/>
+        {/* <input type="time" name="limittime" list="limittimeslist" step="0.001"></input> */}
+        Type <select onChange={this.handleChange('run_type')}>
+          <option>run</option>
+          <option>walk</option>
+          </select>
+        Route <select onChange={this.handleChange('route_id')}>
+          <option >Park Run</option>
+          <option >Short Run</option>
         </select>
-
+        <button>Create Workout</button>
       </form>
     </div>
   )
